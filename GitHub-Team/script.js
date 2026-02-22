19b763f46f97fd9429a1a10e5d747bfae1024056
@cartoon_kachai <126561136+151658ugb@users.noreply.github.com>
2026-02-23 04:06:35 +0700

    Delete GitHub-Team directory

--- GitHub-Team/script.js
+++
@@ -1,8 +0,0 @@
-const levelButtons = document.querySelectorAll(".level-buttons button");
-
-levelButtons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    const level = btn.getAttribute("data-level");
-    window.location.href = `memory-game.html?level=${level}`;
-  });
-});
